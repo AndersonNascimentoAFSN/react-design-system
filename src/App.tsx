03cce342ead482f3d277cbe9fe0f3d2722094ecd
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-13 21:20:39 -0300

    import font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-bold text-2xl text-violet-500">Hello World!</h1>
+
+      <button className="bg-violet-500 front-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
     </div>
   );
 }
