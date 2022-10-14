199acb12a00bf9df6a6fd8886156726531196c9d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-13 21:56:30 -0300

    add stylesguide colors and fontSizes tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 export function App() {
   return (
     <div>
-      <h1 className="text-bold text-2xl text-violet-500">Hello World!</h1>
+      <h1 className="text-bold text-2xl text-cyan-500">Hello World!</h1>
 
-      <button className="bg-violet-500 front-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+      <button className="bg-cyan-500 front-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
         Enviar
       </button>
     </div>
