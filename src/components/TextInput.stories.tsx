423cf9c22849f9d207922c191256be2b1e64198b
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-14 21:09:12 -0300

    adjust text input component

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -25,3 +25,11 @@ export default {
 } as Meta<TextInputRootProps>;
 
 export const Default: StoryObj<TextInputRootProps> = {};
+
+
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: <TextInputs.Input placeholder="Type your e-mail address" />,
+  },
+};
